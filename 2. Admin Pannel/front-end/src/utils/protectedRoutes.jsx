688ab2a94418d522b5885992bf1e458db3276596
Ajay Kumar <126579063+AjayKumar0786@users.noreply.github.com>
2024-09-04 22:50:10 +0530

    Delete 2. Admin Pannel/front-end directory

--- 2. Admin Pannel/front-end/src/utils/protectedRoutes.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { Outlet, Navigate } from 'react-router-dom';
-
-const ProtectedRoutes = () => {
-    const user = sessionStorage.getItem('token'); 
-    return user ? <Outlet /> : <Navigate to="/" />;
-};
-
-export default ProtectedRoutes;
